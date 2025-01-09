2d1e6124ec3926b0b73d68f510fe480dfb3ccc11
Pierre Lestringant <pierre.lestringant@gmail.com>
2025-01-09 20:49:14 +0100

    [Minor] prepare migration to Gtk3 gtk_widget_ref -> g_object_ref

--- src/interface.c
+++ src/interface.c
@@ -30,12 +30,12 @@ Foundation; version 2 of the licence.
 #include "support.h"
 #include "completion.h"
 
-#define GLADE_HOOKUP_OBJECT(component,widget,name) \
-    g_object_set_data_full (G_OBJECT (component), name, \
-        gtk_widget_ref (widget), (GDestroyNotify) gtk_widget_unref)
+#define GLADE_HOOKUP_OBJECT(component, widget, name) \
+    g_object_set_data_full(G_OBJECT(component), name, \
+        g_object_ref(G_OBJECT(widget)), (GDestroyNotify) g_object_unref)
 
-#define GLADE_HOOKUP_OBJECT_NO_REF(component,widget,name) \
-    g_object_set_data (G_OBJECT (component), name, widget)
+#define GLADE_HOOKUP_OBJECT_NO_REF(component, widget, name) \
+    g_object_set_data(G_OBJECT(component), name, widget)
 
 GtkWidget*
 create_main (void)
